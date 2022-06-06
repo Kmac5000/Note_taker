240d02786a9f7d7aa9b1803ce801e4d947596cfb
Kmac5000 <102488109+Kmac5000@users.noreply.github.com>
2022-06-06 17:43:39 -0600

    write and validate

--- server.js
+++ server.js
@@ -1,54 +1,57 @@
 const express = require("express");
-const path = require("path");
 const fs = require("fs");
-const notesJS = require("./public/routes/notesJS");
-
-const PORT = 3001;
+const path = require("path");
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.json());
 app.use(express.static("public"));
-// app.use("./api", api);
-// app.use("./public/routes/notesJS", notesJS);
+
 const { notes } = require("./db/db.json");
 
-function newNote(body, userInput) {
-  let note = body;
-  userInput.push(note);
+function newNote(body, notesArray) {
+  const note = body;
+  notesArray.push(note);
 
-  fs.writeFileSync(path.join(__dirname, "./db/db.json")),
-    JSON.stringify({ notes: notesArray }, null, 2);
-
+  fs.writeFileSync(
+    path.join(__dirname, "./db/db.json"),
+    JSON.stringify({ notes: notesArray }, null, 2)
+  );
   return note;
 }
 
+function validateNote(note) {
+  if (!note.title || typeof note.title !== "string") {
+    return false;
+  }
+  if (!note.text || typeof note.text !== "string") {
+    return false;
+  }
+  return true;
+}
+
 app.get("/api/notes", (req, res) => {
-  req.body.id = notes.length.toString();
-
-  const note = newNote(req.body, notes);
-
-  res.json(note);
+  res.json(notes);
 });
 
 app.post("/api/notes", (req, res) => {
-  const note = newNote(req.body, notes);
-  res.json(note);
+  req.body.id = notes.length.toString();
+  if (validateNote(req.body)) {
+    const note = newNote(req.body, notes);
+    res.json(note);
+  }
 });
 
-// route to homepage
-
-app.get("/api/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-app.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "notes.html"))
-);
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
 
-module.exports = app;
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
