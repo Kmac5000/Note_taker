6ec8aa4f08bde7b224a7c31df0ebbf23e9b5b8e5
Kmac5000 <102488109+Kmac5000@users.noreply.github.com>
2022-06-06 18:03:21 -0600

    routes working

--- routes/note.js
+++ routes/note.js
@@ -1,15 +1,6 @@
-const express = require("express");
+const notes = require("express").Router();
 const fs = require("fs");
 const path = require("path");
-const PORT = 3001;
-
-const app = express();
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static("public"));
-
-const { notes } = require("./db/db.json");
 
 function newNote(body, notesArray) {
   const note = body;
@@ -32,11 +23,11 @@ function validateNote(note) {
   return true;
 }
 
-app.get("/api/notes", (req, res) => {
+notes.get("/api/notes", (req, res) => {
   res.json(notes);
 });
 
-app.post("/api/notes", (req, res) => {
+notes.post("/api/notes", (req, res) => {
   req.body.id = notes.length.toString();
   if (validateNote(req.body)) {
     const note = newNote(req.body, notes);
@@ -44,14 +35,12 @@ app.post("/api/notes", (req, res) => {
   }
 });
 
-app.get("/", (req, res) => {
+notes.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-app.get("/notes", (req, res) => {
+notes.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`API server now on port ${PORT}!`);
-});
+module.exports = notes;
